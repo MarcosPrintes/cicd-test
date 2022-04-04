443a9a763aa8ca9f6bf9083a428ad880bd4bf99b
Marcos Printes <marcosp.printes@hotmail.com>
2022-04-04 16:42:35 -0300

    Adds tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,35 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+/* eslint-disable testing-library/no-debugging-utils */
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter  from '@wojtekmaj/enzyme-adapter-react-17';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+Enzyme.configure({
+  adapter: new EnzymeAdapter(),
+})
+
+test('renders without error', () => {
+  const wrapper = shallow(<App />);
+
+  const appComponent = wrapper.find("[data-test='component-app']");
+
+  expect(appComponent.length).toBe(1);
 });
+
+test('renders increment button', () => {
+
+});
+
+test('renders couter display', () => {
+
+});
+
+test('counter display starts at 0', () => {
+
+});
+
+test('clicking button increments on display', () => {
+
+});
+
+
